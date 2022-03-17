77f804c3ca3b3e27faa493813010ccf1a999a243
Vishal Patel <vishalpe.wa@gmail.com>
2022-03-17 10:54:32 +0530

    added missing changes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const logger = require('./lib/logger');
 const mongoose = require("mongoose");
 const { HOST, DATABASE, MONGODB_PORT, NODE_PORT, MONGODB_URL } = require('./config/db.config');
 const routes = require('./src/routes/index');
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 const db = require("./src/models");
@@ -23,14 +25,14 @@ process.on('uncaughtException', function (err) {
   logger.error('Caught exception: ' + err)
 });
 
-app.use('/user', routes.user);
-app.use('/shop', routes.shop);
-app.use('/category', routes.category);
-app.use('/product-item', routes.productItem);
-app.use('/vendor', routes.vendor);
-app.use('/cart', routes.cart);
-app.use('/brand', routes.brand);
-app.use('/wishlist', routes.wishlist);
+app.use('/api/user', routes.user);
+app.use('/api/shop', routes.shop);
+app.use('/api/category', routes.category);
+app.use('/api/product-item', routes.productItem);
+app.use('/api/vendor', routes.vendor);
+app.use('/api/cart', routes.cart);
+app.use('/api/brand', routes.brand);
+app.use('/api/wishlist', routes.wishlist);
 
 // Connect to Mongoose and set connection variable
 mongoose.connect('mongodb+srv://test-user:test-user@cluster0.unxi0.mongodb.net/iKonMall?retryWrites=true&w=majority', { useNewUrlParser: true });
@@ -38,7 +40,7 @@ mongoose.connect('mongodb+srv://test-user:test-user@cluster0.unxi0.mongodb.net/i
 var dbMongoose = mongoose.connection;
 // Added check for DB connection
 // define a root route
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   if (!dbMongoose)
     res.send("Error connecting db")
   else
@@ -50,6 +52,8 @@ var port = process.env.PORT || NODE_PORT;
 
 // cron for deleting cart items
 require('./src/services/cron.service');
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // listen for requests
 app.listen(port, () => {
